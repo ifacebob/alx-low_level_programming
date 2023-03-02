f69fc6cbd29c4fdce606487274c7a515b554481b
ifacebob <ugochukwubobby@gmail.com>
2023-03-02 12:15:56 -0800

    Write a function that capitalizes all words of a string.
        Prototype: char *cap_string(char *);

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,7 +4,7 @@
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
  *
- Return: A pointer to the changed string.
+ * Return: A pointer to the changed string.
  */
 
 char *cap_string(char *str)
@@ -15,43 +15,22 @@ char *cap_string(char *str)
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-
-                if (str[index - 1] == ' ' ||
-
-                    str[index - 1] == '\t' ||
-
-                    str[index - 1] == '\n' ||
-
-                    str[index - 1] == ',' ||
-
-                    str[index - 1] == ';' ||
-
-                    str[index - 1] == '.' ||
-
-                    str[index - 1] == '!' ||
-
-                    str[index - 1] == '?' ||
-
-                    str[index - 1] == '"' ||
-
-                    str[index - 1] == '(' ||
-
-                    str[index - 1] == ')' ||
-
-                    str[index - 1] == '{' ||
-
-                    str[index - 1] == '}' ||
-
-                    index == 0)
-
-                        str[index] -= 32;
-
-                index++;
-
-        }
-
-        return (str);
-
+		if (str[index - 1] == ' ' ||
+		str[index - 1] == '\t' ||
+		str[index - 1] == '\n' ||
+		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
+		str[index - 1] == '.' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
+		index == 0)
+			str[index] -= 32;
+		index++;
+	}
+	return (str);
 }
-
-
